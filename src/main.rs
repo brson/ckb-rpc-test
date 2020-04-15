c729ade87b6a9df9e424010a980056c934e289a7
Brian Anderson <andersrb@gmail.com>
2020-04-15 22:46:30 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use structopt::StructOpt;
+use anyhow::Result;
+
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::from_args();
+    panic!()
 }
