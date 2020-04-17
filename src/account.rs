64bd9cee3d87a13e14aade1f547a06461eb26adc
Brian Anderson <andersrb@gmail.com>
2020-04-17 04:43:56 +0000

    .

--- src/account.rs
+++ src/account.rs
@@ -1,14 +1,19 @@
 use anyhow::Result;
+use ckb_sdk::wallet::{MasterPrivKey, Key};
 
 use crate::GlobalOpts;
 
 pub type AccountName = String;
 
-pub struct Account;
+pub struct Account {
+    key: Key,
+}
 
 impl Account {
     pub fn create(opts: &GlobalOpts, name: &AccountName) -> Result<Account> {
-        panic!()
+        let privkey = MasterPrivKey::try_new(1024)?;
+        let key = Key::new(privkey);
+        Ok(Account { key })
     }
 
     pub fn load(opts: &GlobalOpts, name: &AccountName) -> Result<Account> {
